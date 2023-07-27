ee5387301ee1af7b0f92629690dd02e72605b5e6
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-07-27 16:50:55 +0900

    message

---
+++ src/main/java/com/personalHomepage/demo/PersonalHomepageV2Application.java
@@ -0,0 +1,13 @@
+package com.personalHomepage.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalHomepageV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalHomepageV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalHomepage/demo/PersonalHomepageV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalHomepage.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalHomepageV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
