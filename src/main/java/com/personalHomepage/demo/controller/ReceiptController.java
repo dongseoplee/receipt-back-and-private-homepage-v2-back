96f70dcd135b9d99823e2d0f4cc079e76242ed95
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2023-12-12 21:32:19 +0900

    post method json

--- src/main/java/com/personalHomepage/demo/controller/ReceiptController.java
+++ src/main/java/com/personalHomepage/demo/controller/ReceiptController.java
@@ -5,10 +5,7 @@ import com.personalHomepage.demo.dto.ReceiptDTO;
 import com.personalHomepage.demo.service.ReceiptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -18,8 +15,8 @@ public class ReceiptController {
     private final ReceiptService receiptService;
 
     @PostMapping("/receipt/image")
-    public String receiptPost(ReceiptDTO receiptDTO) {
-//        System.out.println(receiptDTO);
+    public String receiptPost(@RequestBody ReceiptDTO receiptDTO) {
+        System.out.println(receiptDTO);
         receiptService.saveReceipt(receiptDTO);
         return "success";
     }
